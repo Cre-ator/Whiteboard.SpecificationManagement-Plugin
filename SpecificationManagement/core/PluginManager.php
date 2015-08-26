42c520e1192fe9fd862ba769f77a2f29fd22c9a4
Stefan Schwarz <s.schwarz@online.de>
2015-08-26 14:22:32 +0200

    updated sql strings

--- SpecificationManagement/core/PluginManager.php
+++ SpecificationManagement/core/PluginManager.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * core functions needed in the plugin several times
- * 
- * @author schwarz
- *
- */
 class PluginManager
 {
 	public function getActMantisVersion()
@@ -15,10 +9,10 @@ class PluginManager
 	
 	public function updateReqResInBugtableByBugId( $requirement, $resource, $bugId )
 	{
-		$sqlquery = 'UPDATE mantis_bug_table';
-		$sqlquery = $sqlquery .= ' SET requirement =' . '"' . $requirement . '",';
-		$sqlquery = $sqlquery .= ' resource =' . '"' . $resource . '"';
-		$sqlquery = $sqlquery .= ' WHERE mantis_bug_table.id = ' . $bugId;
+		$sqlquery = ' UPDATE mantis_bug_table' .
+						' SET requirement =' . '"' . $requirement . '",' .
+						' resource =' . '"' . $resource . '"' .
+						' WHERE mantis_bug_table.id = ' . $bugId;
 		
 		$applyUpdate = db_query( $sqlquery );
 		
@@ -27,13 +21,13 @@ class PluginManager
 	
 	public function getSubprojectByCurrentProject( $actProject )
 	{
-		$sqlquery = 'SELECT mantis_project_table.name AS ""';
-		$sqlquery = $sqlquery .= ' FROM mantis_project_table, mantis_project_hierarchy_table';
-		$sqlquery = $sqlquery .= ' WHERE mantis_project_table.id = (';
-		$sqlquery = $sqlquery .= ' SELECT mantis_project_hierarchy_table.child_id';
-		$sqlquery = $sqlquery .= ' FROM mantis_project_hierarchy_table';
-		$sqlquery = $sqlquery .= ' WHERE mantis_project_hierarchy_table.parent_id = ' . $actProject;
-		$sqlquery = $sqlquery .= ' )';
+		$sqlquery = ' SELECT mantis_project_table.name AS ""' .
+						' FROM mantis_project_table, mantis_project_hierarchy_table' .
+						' WHERE mantis_project_table.id = (' .
+						' SELECT mantis_project_hierarchy_table.child_id' .
+						' FROM mantis_project_hierarchy_table' .
+						' WHERE mantis_project_hierarchy_table.parent_id = ' . $actProject .
+						' )';
 		
 		$subprojectByCurrentProject = db_query( $sqlquery );
 		
@@ -42,17 +36,17 @@ class PluginManager
 	
 	public function getValidBugsByCurrentProject( $actProject )
 	{		
-		$sqlquery = 'SELECT *';
-		$sqlquery = $sqlquery .= ' FROM mantis_bug_table';
-		$sqlquery = $sqlquery .= ' WHERE (NOT mantis_bug_table.requirement = ""';
-		$sqlquery = $sqlquery .= ' OR NOT mantis_bug_table.requirement = null';
-		$sqlquery = $sqlquery .= ' OR NOT mantis_bug_table.resource = ""';
-		$sqlquery = $sqlquery .= ' OR NOT mantis_bug_table.resource = null)';
+		$sqlquery = ' SELECT *' .
+						' FROM mantis_bug_table' .
+						' WHERE (NOT mantis_bug_table.requirement = ""' .
+						' OR NOT mantis_bug_table.requirement = null' .
+						' OR NOT mantis_bug_table.resource = ""' .
+						' OR NOT mantis_bug_table.resource = null)';
 		if ( $actProject != '' )
 		{
-			$sqlquery = $sqlquery .= ' AND mantis_bug_table.project_id = ' . $actProject;
+			$sqlquery .= ' AND mantis_bug_table.project_id = ' . $actProject;
 		}
-		$sqlquery = $sqlquery .= ' ORDER BY mantis_bug_table.last_updated DESC';
+		$sqlquery .= ' ORDER BY mantis_bug_table.last_updated DESC';
 		
 		$allValidBugsByCurrentProject = db_query( $sqlquery );
 		
@@ -61,9 +55,9 @@ class PluginManager
 	
 	public function getCategoryNameById( $categoryId )
 	{
-		$sqlquery = 'SELECT DISTINCT mantis_category_table.name AS ""';
-		$sqlquery = $sqlquery .= ' FROM mantis_category_table, mantis_bug_table';
-		$sqlquery = $sqlquery .= ' WHERE mantis_category_table.id = ' . $categoryId;
+		$sqlquery = ' SELECT DISTINCT mantis_category_table.name AS ""' .
+						' FROM mantis_category_table, mantis_bug_table' .
+						' WHERE mantis_category_table.id = ' . $categoryId;
 		
 		$categoryName = db_query( $sqlquery );
 		

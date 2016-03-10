e86c0d045a817edd0ecc5d947d0f1a66f540067b
Stefan Schwarz <s.schwarz@online.de>
2016-03-10 15:02:49 +0100

    fix of inconsistent language strings

--- SpecManagement/pages/version_graph.php
+++ SpecManagement/pages/version_graph.php
@@ -20,7 +20,7 @@ function calculate_page_content( $print_flag )
 {
    $specmanagement_print_api = new specmanagement_print_api();
 
-   html_page_top1( plugin_lang_get( 'select_doc_title' ) );
+   html_page_top1( plugin_lang_get( 'menu_versgraph' ) );
    echo '<link rel="stylesheet" href="' . SPECMANAGEMENT_FILES_URI . 'specmanagement.css">';
    if ( !$print_flag )
    {
